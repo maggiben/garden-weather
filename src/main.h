6e7224355522c48003b6d1303ab696f5022510d1
Benjamin <benjaminmaggi@gmail.com>
2025-04-25 15:25:06 -0300

    fix: no raw

--- src/main.h
+++ src/main.h
@@ -109,12 +109,10 @@ struct CommandHandler {
 void serialPortHandler(void *pvParameters);
 void handleUnknown();
 void handleGetSensor(const String& command);
-void handleRawSensor(const String& command);
 
 // Array of command handlers
 CommandHandler commandHandlers[] = {
     {"get-sensor", handleGetSensor},
-    {"raw-sensor", handleRawSensor},
     // Add other commands and handlers here
 };
 
